127b340712d151c942bbe8f4fb58feb6ef04543d
sailakshmy <sailakshmy94@gmail.com>
2021-08-23 10:05:14 +0530

    Commit-2: Created an assets folder with all the language details available for the Google Dictionary Api, passed the word and language as props to the Header component, created the required inputs and handled onChange methods for both the inputs, created style rules for Header and title

--- src/App.js
+++ src/App.js
@@ -6,9 +6,10 @@ import Header from './components/Header/Header';
 function App() {
   const [word, setWord] = useState('');
   const [meanings, setMeanings] = useState([]);
+  const [language, setLanguage]= useState('en');
   const dictionaryApiCall = async()=>{
     try {
-      const data = await axios.get('https://api.dictionaryapi.dev/api/v2/entries/en/bible');
+      const data = await axios.get(`https://api.dictionaryapi.dev/api/v2/entries/${language}/bible`);
       setMeanings(data.data);
     } catch (error) {
       console.log(error);
@@ -21,7 +22,7 @@ function App() {
   return (
     <div className="App" style={{height: '100vh', backgroundColor:'#282c34', color:'white'}}>
       <Container maxWidth='sm' style={{display:'flex', flexDirection: 'column', height:'100vh'}}>
-        <Header/>
+        <Header language={language} setLanguage={setLanguage} word={word} setWord={setWord}/>
       </Container>
     </div>
   );
--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,10 +1,46 @@
 import React from 'react';
+import { createTheme, ThemeProvider } from '@material-ui/core/styles';
+import { TextField, MenuItem } from '@material-ui/core';
+import languages from '../../assets/languages';
 import './Header.css';
 
-const Header = () => {
+const Header = ({language, setLanguage, word, setWord}) => {
+  const darkTheme = createTheme({
+    palette: {
+      primary:{
+        main:'#fff'
+      },
+      type: 'dark',
+    },
+  });
+  // console.log("language", language);
   return (
     <div className='header'>
       <span className='title'>Word-Search</span>
+      <div className="inputs">
+        <ThemeProvider theme={darkTheme}>
+          <TextField 
+          className='search' 
+          label="Search the word" 
+          variant="outlined" 
+          value={word}
+          onChange={(e)=>setWord(e.target.value)} />
+          <TextField
+          id="outlined-select-language"
+          select
+          value={language}
+          onChange={(e)=>setLanguage(e.target.value)}
+          label='Language'
+          variant="outlined"
+        >
+          {languages.map((option) => (
+            <MenuItem key={option.label} value={option.label}>
+              {option.value}
+            </MenuItem>
+          ))}
+        </TextField>
+        </ThemeProvider>
+      </div>
     </div>
   )
 }
