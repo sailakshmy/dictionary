cca9edb083c3bd39e71576b588b47532d9dd66c9
sailakshmy <sailakshmy94@gmail.com>
2021-08-25 09:57:00 +0530

    Commit-4:Added style rules for Header and Definitions components, Added audio element to handle phonetics.

--- src/components/Definitions/Definitions.jsx
+++ src/components/Definitions/Definitions.jsx
@@ -4,22 +4,32 @@ import './Definitions.css';
 const Definitions = ({word, definitions, language}) => {
   return (
     <div className='meanings'>
-     {word===''
+        {console.log(definitions)}
+        {word && language==='en' && definitions[0] && definitions[0].phonetics[0] && (
+            <audio 
+            style={{backgroundColor:'#fff', borderRadius: 10}} 
+            src={definitions[0].phonetics[0].audio}
+            controls 
+            type="audio/mp3">
+                Sorry. Your browser does not support audio.
+            </audio>
+        )}
+     {word ===''
      ?<span className='noWord'>Start by typing a word in the Search bar</span>
     : definitions.map(definition=>(
         definition.meanings.map(meaning=>(
             meaning.definitions.map(def=>(
                 <div className="singleDefinition" style={{backgroundColor:'white', color:'black'}}>
                     <b>{def.definition}</b>
+                    <hr style={{backgroundColor:'black', width:'100%'}}/>
                     <br/>
                     {def.example && <span>
                         <b><u>Example:</u> {def.example}</b>
                         </span>}
                         <br/>
-                    {def.synonyms && <span>
+                    {def.synonyms?.length>0 && <span>
                         <b><u>Synonyms:</u>{def.synonyms.join(',')}</b>
                         </span>}
-                    <hr style={{backgroundColor:'black', width:'100%'}}/>
                 </div>
             ))
         ))
